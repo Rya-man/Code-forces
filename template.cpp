2a44e8e6c6deb231f3d90bb6c70404548e40254f
Aryaman Prakash <arpr234@gmail.com>
2024-03-28 01:27:37 +0530

    commit changes

--- template.cpp
+++ template.cpp
@@ -1,9 +1,21 @@
 #include<bits/stdc++.h>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define ll long long
+#define endl "\n"
+#define show(arr) for (auto i: arr) {cout << i << ' ';} cout << "\n"
+#define all(v) (v).begin(),(v).end()
+#define rall(v) (v).rbegin(),(v).rend()
+#define debug(x) cout << #x << " = " << (x) << endl;
 using namespace std;
-int main()
-{
-    ios::sync_with_stdio(false);
+auto initialize = []() {
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
+    return 0;
+}();
+int main()
+{
+    
 }
