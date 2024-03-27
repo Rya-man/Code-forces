0c37dd6c7b90092133c326660625e9d5bce0ca3c
Aryaman Prakash <arpr234@gmail.com>
2024-03-28 02:14:00 +0530

    git push

--- template.cpp
+++ template.cpp
@@ -1,21 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-#define ll long long
-#define endl "\n"
-#define show(arr) for (auto i: arr) {cout << i << ' ';} cout << "\n"
-#define all(v) (v).begin(),(v).end()
-#define rall(v) (v).rbegin(),(v).rend()
-#define debug(x) cout << #x << " = " << (x) << endl;
 using namespace std;
+
+// Typedefs
+#define ll long long
+#define vi vector<int>
+#define pii pair<int, int>
+
+// Constants
+const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
+
+// Macros
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+
+// Debugging
+#ifdef LOCAL_DEBUG
+#define debug(x) cerr << #x << " = " << x << endl;
+#else
+#define debug(x)
+#endif
+
+// Fast I/O
 auto initialize = []() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     return 0;
 }();
-int main()
-{
-    
+
+// Modular arithmetic
+ll mod(ll a, ll m) {
+    return (a % m + m) % m;
+}
+
+// Power function with modulo
+ll power(ll x, ll y, ll m) {
+    if (y == 0)
+        return 1;
+    ll p = power(x, y / 2, m) % m;
+    p = (p * p) % m;
+    return (y % 2 == 0) ? p : (x * p) % m;
+}
+
+// Prime number check
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+
+int main() {
+
+
+    // Your code here
+
+    return 0;
 }
