a4ec0aea5295e2c9e4e4ac4488c9a64d135e87a4
Aryaman Prakash <arpr234@gmail.com>
2024-04-18 00:17:38 +0530

    commit changes

--- 102B.cpp
+++ 102B.cpp
@@ -75,28 +75,38 @@ ll sum_of_array(vi arr,int n)
         sum+=arr[i];
     return sum;
 }
-
-int checknum(int a,int& count)
+ll func(ll n)
 {
-    if(a<10)
-    return count;
-
-    count++;
-    int x=0;
-
-    while(a)
+    ll sum=0;
+    while(n>0)
     {
-        x+=a%10;
-        a/=10;
+        sum+=n%10;
+        n/=10;
     }
-    return checknum(x,count);
-} 
+    return sum;
+}
+
 void result(){
-    int a;
-    cin>>a;
-    int count=0;
-    checknum(a,count);
-    cout<<count;
+ ll m,n,t,b,c,d,i,j,k,x,y,z,ans,cnt=1,sum=0;
+    string s,s1;
+    cin>>s;
+    if(s.size()!=1)
+    {
+        for(i=0; i<s.size(); i++)
+        {
+            sum+=(s[i]-'0');
+        }
+
+        while(sum>=10)
+        {
+            ans=sum;
+            sum=func(ans);
+            cnt++;
+        }
+        cout<<cnt<<endl;
+    }
+    else
+        cout<<"0"<<endl;
 }
 
 int main() {
